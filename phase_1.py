a041156cd19d97f032b592f2ad50d4685c83d847
hyeladzirajames <adzirajbalami@gmail.com>
2024-12-15 23:23:34 +0000

    modified phase 1 and 2

--- phase_1.py
+++ phase_1.py
@@ -11,7 +11,7 @@
 
 #imports
 import sqlite3
-import pandas as pd
+import os
 from datetime import datetime, timedelta
 
 
@@ -28,20 +28,14 @@ def select_all_countries(connection):
         # Execute the query via the cursor object.
         result = cursor.execute(query)
 
-        # Define the columns with column names in the table called countries from the database
-        columns = ["id", "name", "timezone"]
-
-        # Creating a dataframe to map it to the columns before iterating
-        df = pd.DataFrame(result, columns=columns)
-        print(df)
-
         # Iterate over the results and display the results.
-        for _, row in df.iterrows():
-            print(f"Country Id: {row['id']} -- Country Name: {row['name']} -- Country Timezone: {row['timezone']}")
-            return result
+        for row in result:
+           print(f"Country Id: {row['id']} -- Country Name: {row['name']} -- Country Timezone: {row['timezone']}")
+        return result
                
     except sqlite3.OperationalError as ex:
-        print(ex)
+        print(f"SQL Error: {str(ex)}")
+
 
 
 
@@ -60,33 +54,32 @@ def select_all_cities(connection):
         # Execute the query via the cursor object.
         result = cursor.execute(query)
 
-        # Define the columns with column names in the table called cities from the database
-        columns = ["id", "name", "longitude", "latitude", "country_id"]
-
-        # Creating a dataframe to map it to the columns before iterating
-        df = pd.DataFrame(result, columns=columns)
-        print(df)
-
         # Iterate over the results and display the results.
-        for _, row in df.iterrows():
-            print(f"City Id: {row['id']} -- City Name: {row['name']} -- City Longitude: {row['longitude']} -- City Latitude: {row['latitude']} -- City Country_id: {row['country_id']}")
-            return result
+        for row in result:
+
+            longitude = float(row['longitude'])
+            latitude = float(row['latitude'])
+
+            print(f"City Id: {row['id']} -- City Name: {row['name']} -- City Longitude: {longitude:.2f} -- City Latitude: {latitude:.2f} -- City Country_id: {row['country_id']}")
+        
+            
+        return result
         
     except sqlite3.OperationalError as ex:
-        print(ex)
-
+        print(f"SQL Error: {str(ex)}")
 
 
 
 
 def average_annual_temperature(connection, city_id, year):
+    # Calculate the average annual temperature for a given city and year.
     try:
         # Get a cursor object from the database connection that will be used to execute database query.
         cursor = connection.cursor()
 
         # Define the query
         query = '''
-            SELECT AVG(dw.mean_temp)
+            SELECT c.name as city_name, AVG(dw.mean_temp) as avg_mean_temp
             FROM cities c
             JOIN daily_weather_entries dw 
             ON c.id = dw.city_id
@@ -94,12 +87,14 @@ def average_annual_temperature(connection, city_id, year):
         '''
 
         # Execute the query via the cursor object and display the results to the console.
-        result = cursor.execute(query, (city_id, str(year))).fetchone()
-        print(f"{result[0]:.2f}")
+        result = cursor.execute(query, (city_id, year)).fetchone()
+
+        print(f"Average annual temperature for city Id {city_id} and City Name {result['city_name']} in {year}: {result['avg_mean_temp']:.2f}°C")
+
         return result
     
     except sqlite3.OperationalError as ex:
-        print(ex)
+         print(f"SQL Error: {str(ex)}")
 
 
 
@@ -116,7 +111,7 @@ def average_seven_day_precipitation(connection, city_id, start_date):
 
         # Define the query
         query = '''
-            SELECT AVG(dw.precipitation)
+            SELECT AVG(dw.precipitation) as avg_precipitation
             FROM cities c
             JOIN daily_weather_entries dw 
             ON c.id = dw.city_id
@@ -125,36 +120,42 @@ def average_seven_day_precipitation(connection, city_id, start_date):
 
         # Execute the query via the cursor object and display the results to the console.
         result = cursor.execute(query, (city_id, start_date, end_date)).fetchone()
-        print(f"{result[0]:.2f}")
+        print(f"Average Seven Day Precipitation for city ID: {city_id} from date {start_date} to date {end_date} is: {result['avg_precipitation']:.2f}mm")
         return result
 
     except sqlite3.OperationalError as ex:
-        print(ex)
+        print(f"SQL Error: {str(ex)}")
 
 
 
 
 
 def average_mean_temp_by_city(connection, date_from, date_to):
+    try:
         # Get a cursor object from the database connection that will be used to execute database query.
         cursor = connection.cursor()
 
         # Define the query 
         query = '''
-            SELECT AVG(dw.mean_temp), c.name
+            SELECT c.name as city_name, AVG(dw.mean_temp) as avg_mean_temp
             FROM cities c
             JOIN daily_weather_entries dw 
             ON c.id = dw.city_id
             WHERE dw.date BETWEEN ? AND ?
-            GROUP BY c.name;
+            GROUP BY c.id;
         '''
 
         # Execute the query via the cursor object and display the results to the console.
         result = cursor.execute(query, (date_from, date_to)).fetchall()
 
         # Iterate over the results and display the results. 
-        for row in result:           print(f"City: {row[0]} -- Avg Temperature: {row[1]}°C")
+        for row in result:
+            #print(dict(row))      
+            print(f"City: {row['city_name']} -- Avg Temperature: {row['avg_mean_temp']:.2f}°C")
         return result
+    
+    except sqlite3.OperationalError as ex:
+        print(f"SQL Error: {str(ex)}")
         
 
 
@@ -162,11 +163,13 @@ def average_mean_temp_by_city(connection, date_from, date_to):
 
 def average_annual_precipitation_by_country(connection, year):
     # Get a cursor object from the database connection that will be used to execute database query.
+
+    try:
         cursor = connection.cursor()
 
         # Define the query 
         query = '''
-            SELECT AVG(dw.precipitation), co.name
+            SELECT co.name as country_name, AVG(dw.precipitation) as avg_precipitation
             FROM countries co
             JOIN cities c 
             ON co.id = c.country_id
@@ -181,59 +184,57 @@ def average_annual_precipitation_by_country(connection, year):
 
         # Iterate over the results and display the results. 
         for row in result:
-            print(f"Country: {row[0]} -- Avg Precipitation: {row[1]:.2f} mm")
+            Country = (row['country_name'])
+            Avg_Precipitation = float(row['avg_precipitation'])
+            print(f"Country: {Country} -- Avg_Precipitation: {Avg_Precipitation:.2f} mm")
         return result
+    
+    except sqlite3.OperationalError as ex:
+        print(f"SQL Error: {str(ex)}")
+        # raise Exception(f"{str(ex)}")
 
 
 
 
 
-# def select_all_countries2(connection):
-#     # Queries the database and selects all the countries stored in the countries table of the database.
-#     # The returned results are then printed to the console.
-#     try:
-#         # Define the query
-#         query = "SELECT * from [countries]"
 
-#         # Get a cursor object from the database connection that will be used to execute database query.
-#         cursor = connection.cursor()
-
-#         # Execute the query via the cursor object.
-#         results = cursor.execute(query)
-
-#         return results
-        
-#     except sqlite3.OperationalError as ex:
-#         raise Exception(f"{str(ex)}")
-
-
-
-
+file_path = os.path.abspath(__file__)
+working_directory = os.path.dirname(file_path)
+os.chdir(working_directory)
+database_location = "CIS4044-N-SDI-OPENMETEO-PARTIAL.db"
 
 if __name__ == "__main__":
-    with sqlite3.connect("/Users/hyeladzirajames/Library/CloudStorage/OneDrive-TeessideUniversity/SECOND SEMESTER/SOFTWARE FOR DIGITAL INNOVATIONS/ICA_DELIVERABLES/D3914083_JAMES.HYELADZIRA/Historical_Weather-Data_App(Open-Meteo-Weather-Data)/CIS4044-N-SDI-OPENMETEO-PARTIAL.db") as conn:
 
+    with sqlite3.connect(database_location) as conn:
+        conn.row_factory = sqlite3.Row
+        
+        print("-----------Select All Countries------------")
         select_all_countries(conn)
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
+        print("----------Select All Cities--------------")
         select_all_cities(conn)
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
-        average_annual_temperature(conn, 1, 2021)
+        print("------------Average Annual Temperature---------------")
+        average_annual_temperature(conn, 1, "2021")
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
+        print("------------Average Seven Day Precipitation----------------")
         average_seven_day_precipitation(conn, 2, "2022-05-16")
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
-        average_mean_temp_by_city(conn, "2022-05-19", "2022-05-30")
+        print("------------Average Mean Temperature By City----------------")
+        average_mean_temp_by_city(conn, "2022-05-19", "2022-05-30")  
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
-        average_annual_precipitation_by_country(conn, 2021)
+        print("------------Average Annual Precipitation By Country-------------")
+        average_annual_precipitation_by_country(conn, "2020")
         print("----------------------------------------------------------------------------------------------------------------")
         print("\n")
 
--- phase_2.py
+++ phase_2.py
@@ -14,19 +14,26 @@
 
 # Imports
 import matplotlib as plt
-import phase_1 as module_code
+import phase_1 as queries
 import sqlite3
+import os
+
+file_path = os.path.abspath(__file__)
+working_directory = os.path.dirname(file_path)
+os.chdir(working_directory)
+database_location = "CIS4044-N-SDI-OPENMETEO-PARTIAL.db"
 
 
 def plot():
-   with sqlite3 .connect("../ICA_database/CIS4044-N-SDI-OPENMETEO-PARTIAL.db") as conn:
-      try:
-      
-        data = module_code.select_all_countries2(conn)
-        # -TODO add plot code to use the data
-
-      except Exception as e:
-        pass
    
+  with sqlite3.connect(database_location) as conn:
+    try:
+        
+      data = queries.select_all_countries2(conn)
+      # -TODO add plot code to use the data
+
+    except Exception as e:
+          pass
+    
       
       
